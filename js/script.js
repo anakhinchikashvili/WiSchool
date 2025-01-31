8a7859916d63a72d3be34c66b91c1dba4c8c9172
anakhinchikashvili <ana.khinchikashvili.1@btu.edu.ge>
2025-01-31 23:36:01 +0400

    Adding HTML structure

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ function toggleMenu() {
     document.querySelector(".ul-item").classList.toggle("active");
 }
 
+let index = 0;
+
+function moveSlide(direction) {
+    const slider = document.querySelector(".fields-container");
+    const slides = document.querySelectorAll(".fav-el");
+    const totalSlides = slides.length;
+    
+    index += direction;
+    if (index < 0) index = totalSlides - 1;
+    if (index >= totalSlides) index = 0;
+
+    const offset = -index * 100 + "%"; 
+    slider.style.transform = `translateX(${offset})`;
+}
+
