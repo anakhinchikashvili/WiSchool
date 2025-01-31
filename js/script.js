eb40ad9b6c3dca97e4cc18789f6accf633f100ff
anakhinchikashvili <ana.khinchikashvili.1@btu.edu.ge>
2025-02-01 01:34:39 +0400

    Corrections in HTML and CSS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-function toggleMenu() {
-    document.querySelector(".ul-item").classList.toggle("active");
-}
+document.getElementById("burgerMenu").addEventListener("click", function() {
+    document.querySelector(".nav-list").classList.toggle("active");
+});
 
 let index = 0;
 
 function moveSlide(direction) {
-    const slider = document.querySelector(".fields-container");
-    const slides = document.querySelectorAll(".fav-el");
-    const totalSlides = slides.length;
-    
+    const slider = document.querySelector(".course-categories");
+    const totalSlides = document.querySelectorAll(".course-el").length;
+
     index += direction;
     if (index < 0) index = totalSlides - 1;
     if (index >= totalSlides) index = 0;
@@ -16,4 +15,7 @@ function moveSlide(direction) {
     const offset = -index * 100 + "%"; 
     slider.style.transform = `translateX(${offset})`;
 }
+
+document.getElementById("prevSlide").addEventListener("click", () => moveSlide(-1));
+document.getElementById("nextSlide").addEventListener("click", () => moveSlide(1));
 
